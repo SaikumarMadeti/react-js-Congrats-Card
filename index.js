35efcfac4039b87e158a19b33090085d5c943a4e
Saikumar <marjunsaikumar@gmail.com>
2023-04-06 11:01:41 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  <div className="bg-container">
+    <div className="card-container">
+      <img
+        className="logo"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+      />
+      <h1>Kiran V</h1>
+      <p>Vishnu Instititude of Computer Education and Technology, Bhimavaram</p>
+      <img
+        className="logo"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
